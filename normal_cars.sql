afb2d9e88ed03bae0d996b0524230c010ebcb7cb
Owen <owenyangg@gmail.com>
2016-04-18 20:31:07 -1000

    solved linkage of models private key with make primary key in single INSERT

--- normal_cars.sql
+++ normal_cars.sql
@@ -28,27 +28,49 @@ CREATE TABLE manufacturers (
 
 INSERT INTO manufacturers (make_title, make_code)
   SELECT DISTINCT make_title, make_code
-  FROM car_models;
+  FROM car_models
+  ORDER BY make_title;
+
+-- SELECT * FROM manufacturers;
 
 -- Models table
 CREATE TABLE models (
   id SERIAL NOT NULL UNIQUE PRIMARY KEY,
   model_title varchar(125),
   model_code varchar(125),
-  manufacturer integer REFERENCES manufacturers,
-  make_title varchar(125),
-  make_code varchar(125)
+  manufacturer integer REFERENCES manufacturers
 );
 
-INSERT INTO models (model_title, model_code, make_title, make_code)
-  SELECT DISTINCT model_title, model_code, make_title, make_code
-  FROM car_models;
 
-SELECT manufacturers.id
-FROM manufacturers, models
-WHERE models.make_title = manufacturers.make_title;
+INSERT INTO models (model_title, model_code, manufacturer)
+  WITH car_models2 AS (
+    SELECT DISTINCT
+    model_title, model_code, make_title
+    FROM car_models
+    ORDER BY model_title ASC
+  ),
+  manufacturers AS (
+    WITH new_car_models AS(
+      SELECT DISTINCT model_title, model_code, make_title, make_code FROM
+      car_models
+      ORDER BY model_title ASC
+      ),
+    manufacturers2 AS(
+      SELECT make_title, make_code, id
+      FROM manufacturers
+      )
+    SELECT manufacturers2.id, manufacturers2.make_title
+    FROM manufacturers2, new_car_models
+    WHERE new_car_models.make_title = manufacturers2.make_title
+      AND new_car_models.make_code = manufacturers2.make_code
+  )
+SELECT DISTINCT
+car_models2.model_title, car_models2.model_code, manufacturers.id
+FROM car_models2 JOIN manufacturers ON
+car_models2.make_title = manufacturers.make_title;
 
-SELECT * FROM models;
+
+SELECT * FROM models LIMIT 25;
 
 -- -- years table
 -- CREATE TABLE years (
